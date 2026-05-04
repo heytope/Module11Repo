c71296e3eaac3f5b52a6f9a08624f947b8278da3
heytope <ttadeolu12@gmail.com>
2026-05-03 21:02:08 -0400

    Added new message

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
 # Temitope Adeolu  
-#5/3/2026
+# 5/3/2026
 # A simple python program to print a message. 
 
+# Temitope Adeolu   
+# 5/3/2026
+# added new message
+
 print("Hello World")
+
+print("Welcome to Github!")
